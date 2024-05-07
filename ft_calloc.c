6f41f2a114632b2534fd896c47d2897ed676cd16
Shintaro Kohtake <skohtake@c2r1s5.42tokyo.jp>
2024-05-07 13:12:29 +0900

    modify calloc strncmp for tester-tokyo

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/30 12:46:07 by skohtake          #+#    #+#             */
-/*   Updated: 2024/05/06 12:37:07 by skohtake         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:58:46 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (size && count > SIZE_MAX / size)
+		return (NULL);
 	ptr = malloc(count * size);
 	if (!ptr)
 		return (NULL);
@@ -28,7 +30,10 @@ void	*ft_calloc(size_t count, size_t size)
 // int	main(void)
 // {
 // 	char	*s;
-// 	s = calloc(5, sizeof(char));
+
+// 	// s = calloc(5, sizeof(char));
+// 	s = calloc(0, 0);
+// 	// s = calloc((size_t)SIZE_MAX / 10 + (size_t)1, 10);
 // 	if (s == NULL)
 // 		printf("Failed\n");
 // 	else
@@ -36,5 +41,20 @@ void	*ft_calloc(size_t count, size_t size)
 // 		s = "1234";
 // 		printf("%s >>> SUCCESS\n", s);
 // 	}
+// 	if (s)
+// 		free(s);
+// 	////////////////////////////////////////////////////////////
+// 	// s = ft_calloc(5, sizeof(char));
+// 	s = ft_calloc(0, 0);
+// 	// s = ft_calloc((size_t)SIZE_MAX / 10 + (size_t)1, 10);
+// 	if (s == NULL)
+// 		printf("Failed\n");
+// 	else
+// 	{
+// 		s = "1234";
+// 		printf("%s >>> SUCCESS\n", s);
+// 	}
+// 	if (s)
+// 		free(s);
 // 	return (0);
 // }
--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: skohtake <skohtake@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/29 07:47:28 by skohtake          #+#    #+#             */
-/*   Updated: 2024/05/07 10:55:32 by skohtake         ###   ########.fr       */
+/*   Updated: 2024/05/07 12:44:51 by skohtake         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (s1[i] && s2[i] && i < n)
+	while (i < n && s1[i] && s2[i])
 	{
 		if (s1[i] != s2[i])
 			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
@@ -36,17 +36,19 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 // int	main(void)
 // {
 // 	char	*s1;
-// 	char	*s2;
-// 	char	*s3;
 
+// 	// char	*s2;
+// 	// char	*s3;
 // 	s1 = "Hello,world!";
-// 	s2 = "Hello,42Tokyo!";
-// 	s3 = "Hello,world!!!";
+// 	// s2 = "Hello,42Tokyo!";
+// 	// s3 = "Hello,world!!!";
 // 	printf("Expected:Actual\n");
-// 	printf("%d	:%d\n", strncmp(s1, s2, 5), ft_strncmp(s1, s2, 5));
-// 	printf("%d	:%d\n", strncmp(s1, s2, 6), ft_strncmp(s1, s2, 6));
-// 	printf("%d	:%d\n", strncmp(s1, s2, 7), ft_strncmp(s1, s2, 7));
-// 	printf("%d	:%d\n", strncmp(s1, s2, 100), ft_strncmp(s1, s2, 100));
-// 	printf("%d	:%d\n", strncmp(s1, s3, 100), ft_strncmp(s1, s3, 100));
+// 	printf("%d	:", strncmp(s1, NULL, 0));
+// 	printf("%d\n", ft_strncmp(s1, NULL, 0));
+// 	// printf("%d	:%d\n", strncmp(s1, s2, 5), ft_strncmp(s1, s2, 5));
+// 	// printf("%d	:%d\n", strncmp(s1, s2, 6), ft_strncmp(s1, s2, 6));
+// 	// printf("%d	:%d\n", strncmp(s1, s2, 7), ft_strncmp(s1, s2, 7));
+// 	// printf("%d	:%d\n", strncmp(s1, s2, 100), ft_strncmp(s1, s2, 100));
+// 	// printf("%d	:%d\n", strncmp(s1, s3, 100), ft_strncmp(s1, s3, 100));
 // 	return (0);
 // }
